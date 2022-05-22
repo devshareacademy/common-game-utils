8067b0a711f5cff63c36c51b79bfe86a4e4d68ca
Scott Westover <scottwestover2006@gmail.com>
2022-05-21 22:44:27 -0400

    changed: cleaned up core index file

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,6 @@ import * as array from './array';
 import * as link from './link';
 import * as mobile from './mobile';
 import * as random from './random';
-// import * as storage from './storage';
-
-// export { array, grid, link, mobile, random, storage };
-
-// import * as grid from './grid';
-
-// import { grid } from './grid';
 
 export type { Coordinate, Cell } from './grid';
 export type { Storage } from './storage';
